59cfd507339a25466dfa2553cc2d0aa761db1eea
Parker Foord <700214020@fvtc.edu>
2019-11-07 12:15:22 -0600

    done

---
+++ ObjectTimerUI/PF.ObjectTimer.BL/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace PF.ObjectTimer.BL
+{
+  
+    public class Stopwatch
+    {
+        DateTime startTime = new DateTime();
+
+
+
+        public string ET()
+        {
+         
+            TimeSpan elapsedTime = new TimeSpan();
+
+          
+            elapsedTime = DateTime.Now - startTime;
+            return elapsedTime.ToString(@"hh\:mm\:ss");
+
+        }
+    }
+}
--- ObjectTimerUI/PF.ObjectTimerUI/Form1.cs
+++ ObjectTimerUI/PF.ObjectTimerUI/Form1.cs
@@ -7,14 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PF.ObjectTimer.BL;
 
 namespace PF.ObjectTimerUI
 {
     public partial class Form1 : Form
     {
 
-        DateTime startTime = new DateTime();
-        TimeSpan elapsedTime = new TimeSpan();
+        //Stopwatch time = new Stopwatch();
+       DateTime startTime = new DateTime();
+        //TimeSpan elapsedTime = new TimeSpan();
         public Form1()
         {
             InitializeComponent();
@@ -22,26 +24,36 @@ namespace PF.ObjectTimerUI
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            elapsedTime = DateTime.Now - startTime;
-             lblTime.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+            Stopwatch time = new Stopwatch();
+            //elapsedTime = DateTime.Now - startTime;
+            //lblTime.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+
+            DateTime Toc = Convert.ToDateTime(time.ET());
+
+
+              string tic = Convert.ToString(Toc - startTime);
+
+            lblTime.Text = tic;
+
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
             btnStart.Enabled = false;
             btnStop.Enabled = true;
-            //resetBtn.Enabled = true;
+            //btnReset.Enabled = true;
             timer1.Interval = (1000) * (1);
             timer1.Enabled = true;
             timer1.Start();
             startTime = DateTime.Now;
+           
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
             btnStop.Enabled = false;
             btnStart.Enabled = true;
-           // resetBtn.Enabled = true;
+           // btnReset.Enabled = true;
             timer1.Stop();
         }
     }
